69fab9ac4f9b204abcd66cff0806726ff67fe695
Dang Vu Duy <Dev@dangs-macbook-air.local>
2015-12-29 16:55:19 +0700

    Initial Commit

---
+++ UISwitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UISwitch
+//
+//  Created by Dang Vu Duy on 12/29/15.
+//  Copyright © 2015 Dang Vu Duy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
